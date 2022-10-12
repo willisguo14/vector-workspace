b05795eb58e8a4a8fd83e7a161cf83ad8b9df108
Willis Guo <willisguo02@gmail.com>
2022-10-11 22:40:32 -0400

    update

--- vqgan.py
+++ vqgan.py
@@ -2,12 +2,27 @@ def main():
 
     data_splits_f = "/scratch/ssd002/datasets/celeba/Eval/list_eval_partition.txt"
 
+    train_img_ids = []
+    val_img_ids = []
+    test_img_ids = []
+
     with open(data_splits_f, "r") as f:
         lines = f.read().splitlines()
 
         for line in lines:
             img_id, split = line.split()
-            print(f"{img_id} // {split}")
+            
+            if split == '0':
+                print("TRAIN")
+                train_img_ids.append(img_id)
+            elif split == '1':
+                print("VAL")
+                val_img_ids.append(img_id)
+            elif split == '2':
+                print("TEST")
+                test_img_ids.append(img_id)
+            else:
+                print("ERROR")
 
     pass 
 
