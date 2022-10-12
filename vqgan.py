4e8d58dd77919cba06d91c6e4a3c68e2c74cdecf
Willis Guo <willisguo02@gmail.com>
2022-10-11 22:33:36 -0400

    init

---
+++ vqgan.py
@@ -0,0 +1,14 @@
+def main():
+
+    data_splits_f = "/scratch/ssd002/datasets/celeba/Eval/list_eval_partitions.txt"
+
+    with open(data_splits_f, "r") as f:
+        lines = f.read().splitlines()
+
+        img_id, split = lines.split()
+        print(f"{img_id} // {split}")
+
+    pass 
+
+if __name__ == "__main__":
+    main()
