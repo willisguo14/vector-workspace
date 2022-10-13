7df6193c931873c448fcf17fbc920d4a0d8d7371
Willis Guo <willisguo02@gmail.com>
2022-10-12 22:09:10 -0400

    update

--- vqgan.py
+++ vqgan.py
@@ -1,3 +1,19 @@
+import os
+
+def write_data(f_path, img_ids):
+
+    n = len(img_ids)
+
+    img_path = "PATH_TO_IMAGES"
+
+    with open(f_path, "w") as f:
+
+        for i, img_id in enumerate(img_ids):
+            if i != n - 1:
+                f.write(f"{os.path.join(img_path, img_id)}\n")
+            else:
+                f.write(f"{os.path.join(img_path, img_id)}")   
+
 def main():
 
     data_splits_f = "/scratch/ssd002/datasets/celeba/Eval/list_eval_partition.txt"
@@ -13,18 +29,25 @@ def main():
             img_id, split = line.split()
             
             if split == '0':
-                print("TRAIN")
                 train_img_ids.append(img_id)
             elif split == '1':
-                print("VAL")
                 val_img_ids.append(img_id)
             elif split == '2':
-                print("TEST")
                 test_img_ids.append(img_id)
-            else:
-                print("ERROR")
+    
+    train_f = "train.txt"
+    val_f = "val.txt"
+    test_f = "test.txt"
 
-    pass 
+    write_data(train_f, train_img_ids)
+    write_data(val_f, val_img_ids)
+    write_data(test_f, test_img_ids)
+
+
+    # TODO: 
+    # - write train.txt, val.txt, test.txt (w/ full path to images)
+    # - test that preprocess_image() 
+    # - test dataset? 
 
 if __name__ == "__main__":
     main()
